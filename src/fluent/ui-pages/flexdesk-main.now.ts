6d2c31c349e200864e812869ae646bb99b074120
GrandeJipe <jean-philippe.affani@accenture.com>
2026-05-05 13:14:11 +0200

    fix: scope x_acf_tinkitest + scopeId reel + endpoint corrige (x_acf_tinkitest_flex_main.do)

--- src/fluent/ui-pages/flexdesk-main.now.ts
+++ src/fluent/ui-pages/flexdesk-main.now.ts
@@ -4,7 +4,7 @@ import flexdeskPage from '../../client/index.html';
 
 export const flexdesk_main = UiPage({
   $id: Now.ID['flexdesk-main'],
-  endpoint: 'x_wsb_flex_main.do',
+  endpoint: 'x_acf_tinkitest_flex_main.do',
   html: flexdeskPage,
   direct: true,
 });
